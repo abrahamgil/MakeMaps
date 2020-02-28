6fd410c9a6e429885a0833e810e1cbc72e68a592
Abraham <abrahamgil2@gmail.com>
2020-02-28 05:29:23 -0600

    Added elevations to markers on map that are dynamically changed

--- map1.py
+++ map1.py
@@ -4,13 +4,14 @@ import pandas
 data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
 
 map = folium.Map(location=[29.770838, -95.400394], zoom_start=8, tiles="Stamen Terrain")
 
 #adding feature group to keep code more organized later the FG can be edited easier then just adding child
 fg = folium.FeatureGroup(name="My Map")
-for lt, ln in zip(lat, lon):
-    fg.add_child(folium.Marker(location=[lt, ln],popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.Marker(location=[lt, ln],popup=str(el) + " m", icon=folium.Icon(color='green')))
 
 map.add_child(fg)
 
