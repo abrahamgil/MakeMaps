8abc5311f5064ad5d7c6a9b96266b75332015cb3
Abraham <abrahamgil2@gmail.com>
2020-02-28 04:51:09 -0600

    Created a map of volcanoes in USA using data from a csv

--- map1.py
+++ map1.py
@@ -1,11 +1,17 @@
 import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+
 map = folium.Map(location=[29.770838, -95.400394], zoom_start=8, tiles="Stamen Terrain")
 
 #adding feature group to keep code more organized later the FG can be edited easier then just adding child
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location=[29.2, -95.1],popup="Hi I am a Marker", icon=folium.Icon(color='green')))
-fg.add_child(folium.Marker(location=[28.2, -95.1],popup="Hi I am a Marker", icon=folium.Icon(color='green')))
-fg.add_child(folium.Marker(location=[27.2, -95.1],popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+for lt, ln in zip(lat, lon):
+    fg.add_child(folium.Marker(location=[lt, ln],popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+
 map.add_child(fg)
 
 map.save("Map1.html")
