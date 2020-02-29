122dccc034f650e52be375d9b9c5710b87cdb422
Abraham <abrahamgil2@gmail.com>
2020-02-29 03:19:09 -0600

    Created a function that changes the marker color depending on the elevation

--- map1.py
+++ map1.py
@@ -6,12 +6,20 @@ lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
 
-map = folium.Map(location=[29.770838, -95.400394], zoom_start=8, tiles="Stamen Terrain")
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000<= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
+map = folium.Map(location=[29.770838, -95.400394], zoom_start=6, tiles="Stamen Terrain")
 
 #adding feature group to keep code more organized later the FG can be edited easier then just adding child
 fg = folium.FeatureGroup(name="My Map")
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt, ln],popup=str(el) + " m", icon=folium.Icon(color='green')))
+    fg.add_child(folium.Marker(location=[lt, ln],popup=str(el) + " m", icon=folium.Icon(color=color_producer(el))))
 
 map.add_child(fg)
 
