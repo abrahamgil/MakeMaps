df561ee8fac2246cad5496d9c74b04a5faf5ae2f
Abraham <abrahamgil2@gmail.com>
2020-03-02 06:27:25 -0600

    Added layer control feature to enable and disable certain maps

--- map1.py
+++ map1.py
@@ -17,15 +17,22 @@ def color_producer(elevation):
 map = folium.Map(location=[29.770838, -95.400394], zoom_start=6, tiles="Stamen Terrain")
 
 #adding feature group to keep code more organized later the FG can be edited easier then just adding child
-fg = folium.FeatureGroup(name="My Map")
+fgv = folium.FeatureGroup(name="Volcanoes")
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.CircleMarker(location=[lt, ln],popup=str(el) + " m", radius = 6,
+    fgv.add_child(folium.CircleMarker(location=[lt, ln],popup=str(el) + " m", radius = 6,
     fill_color=color_producer(el), color = 'grey', fill_opacity=0.7))
 
-fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(), 
+fgp = folium.FeatureGroup(name="Population")
+
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(), 
 style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] <10000000 
 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
-map.add_child(fg)
+
+
+map.add_child(fgv)
+map.add_child(fgp)
+
+map.add_child(folium.LayerControl())
 
 map.save("Map1.html")
